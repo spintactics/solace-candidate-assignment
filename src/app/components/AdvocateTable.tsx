eba1bdc2e5ce6438d5bc72c76a2fc1722c4c56bb
spintactics <rainfang2014@gmail.com>
2024-11-04 11:54:15 -0500

    feat(eng-02): AG-Grid active

--- src/app/components/AdvocateTable.tsx
+++ src/app/components/AdvocateTable.tsx
@@ -1,4 +1,5 @@
 import { AgGridReact } from 'ag-grid-react'; // React Data Grid Component
+import { ColDef } from 'ag-grid-community';
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the Data Grid
 import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the Data Grid
 
@@ -10,6 +11,16 @@ interface AdvocateTableProps {
 
 export default function AdvocateTable({ filteredAdvocates }: AdvocateTableProps) {
 
+  const colDefs: ColDef<Advocate>[] = [
+    { field: 'firstName' },
+    { field: 'lastName' },
+    { field: 'city'  },
+    { field: 'degree' },
+    { field: 'specialties' },
+    { field: 'yearsOfExperience' },
+    { field: 'phoneNumber' }
+  ];
+
   // TO DO - there should be loading state
 
   // TO DO - give each row it's own unique key
@@ -18,37 +29,49 @@ export default function AdvocateTable({ filteredAdvocates }: AdvocateTableProps)
   // TO DO - the top specialty should be horizontally aligned with the rest of the row
 
   return (
-      <table>
-        <thead>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>City</th>
-            <th>Degree</th>
-            <th>Specialties</th>
-            <th>Years of Experience</th>
-            <th>Phone Number</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredAdvocates.map((advocate) => {
-            return (
-              <tr>
-                <td>{advocate.firstName}</td>
-                <td>{advocate.lastName}</td>
-                <td>{advocate.city}</td>
-                <td>{advocate.degree}</td>
-                <td>
-                  {advocate.specialties.map((s) => (
-                    <div>{s}</div>
-                  ))}
-                </td>
-                <td>{advocate.yearsOfExperience}</td>
-                <td>{advocate.phoneNumber}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+    <div
+      className="ag-theme-alpine-dark"
+      style={{ 
+        height: '50vh',
+        maxHeight: '70vh',
+        width: '100%' }}
+    >
+      <AgGridReact
+          rowData={filteredAdvocates}
+          columnDefs={colDefs}
+      />
+    </div>
+      // <table>
+      //   <thead>
+      //     <tr>
+      //       <th>First Name</th>
+      //       <th>Last Name</th>
+      //       <th>City</th>
+      //       <th>Degree</th>
+      //       <th>Specialties</th>
+      //       <th>Years of Experience</th>
+      //       <th>Phone Number</th>
+      //     </tr>
+      //   </thead>
+      //   <tbody>
+      //     {filteredAdvocates.map((advocate) => {
+      //       return (
+      //         <tr>
+      //           <td>{advocate.firstName}</td>
+      //           <td>{advocate.lastName}</td>
+      //           <td>{advocate.city}</td>
+      //           <td>{advocate.degree}</td>
+      //           <td>
+      //             {advocate.specialties.map((s) => (
+      //               <div>{s}</div>
+      //             ))}
+      //           </td>
+      //           <td>{advocate.yearsOfExperience}</td>
+      //           <td>{advocate.phoneNumber}</td>
+      //         </tr>
+      //       );
+      //     })}
+      //   </tbody>
+      // </table>
   );
 }
